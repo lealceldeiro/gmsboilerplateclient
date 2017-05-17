3349818cdf7b1de27936e295e3185859d6f38064
Asiel <lealceldeiro@gmail.com>
2017-05-17 09:52:23 -0400

    Fix: show hiders set properly

--- client/app/modules/_security/session.ctrl.js
+++ client/app/modules/_security/session.ctrl.js
@@ -35,25 +35,25 @@
         vm.wizard.can = {
             conf: function () { return (has(up.MANAGE_CONFIGURATION)) },
 
-            manageOwnedEntity: function () { return has(up.MANAGE_OWNED_ENTITY) || vm.wizard.show.createOwnedEntity() || vm.wizard.show.readOwnedEntity() },
+            manageOwnedEntity: function () { return has(up.MANAGE_OWNED_ENTITY) || vm.wizard.can.createOwnedEntity() || vm.wizard.can.readOwnedEntity() },
             readOwnedEntity:   function () { return has([up.READ_OWNED_ENTITY, up.READ_ALL_OWNED_ENTITY], true) },
             createOwnedEntity: function () { return has(up.CREATE_OWNED_ENTITY) },
             updateOwnedEntity: function () { return has(up.UPDATE_OWNED_ENTITY) },
             deleteOwnedEntity:  function () { return has(up.DELETE_OWNED_ENTITY) },
 
-            manageUser: function () { return has(up.MANAGE_USER) || vm.wizard.show.createUser() || vm.wizard.show.readUser() },
+            manageUser: function () { return has(up.MANAGE_USER) || vm.wizard.can.createUser() || vm.wizard.can.readUser() },
             readUser:   function () { return has([up.READ_USER, up.READ_ALL_USER], true) },
             createUser: function () { return has(up.CREATE_USER) },
             updateUser: function () { return has(up.UPDATE_USER) },
             deleteUser:  function () { return has(up.DELETE_USER) },
 
-            manageRole: function () { return has(up.MANAGE_ROLE) || vm.wizard.show.createRole() || vm.wizard.show.readRole() },
+            manageRole: function () { return has(up.MANAGE_ROLE) || vm.wizard.can.createRole() || vm.wizard.can.readRole() },
             readRole:   function () { return has(up.READ_ROLE) },
             createRole: function () { return has(up.CREATE_ROLE) },
             updateRole: function () { return has(up.UPDATE_ROLE) },
             deleteRole:  function () { return has(up.DELETE_ROLE) },
 
-            managePermission: function () { return has(up.MANAGE_PERMISSION) || vm.wizard.show.createPermission() || vm.wizard.show.readPermission()  },
+            managePermission: function () { return has(up.MANAGE_PERMISSION) || vm.wizard.can.createPermission() || vm.wizard.can.readPermission() },
             readPermission:   function () { return has(up.READ_PERMISSION) },
             createPermission: function () { return has(up.CREATE_PERMISSION) },
             updatePermission: function () { return has(up.UPDATE_PERMISSION) },
